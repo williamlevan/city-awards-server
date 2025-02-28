c25ed7f08f5aae37684b39a706d8da48ad5f53b5
williamlevan <wlevan@stlcitysc.com>
2025-02-27 22:14:08 -0600

    editing PORT in index.js

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
